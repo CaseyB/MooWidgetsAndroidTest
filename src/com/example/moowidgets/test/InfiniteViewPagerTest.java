f547a9545159c35ff0c4308fff2b2b1e59bb40ec
Casey Borders <TheBeast.13@gmail.com>
2013-12-25 16:11:21 -0500

    No need to getCount when infinite

--- src/com/example/moowidgets/test/InfiniteViewPagerTest.java
+++ src/com/example/moowidgets/test/InfiniteViewPagerTest.java
@@ -70,12 +70,6 @@ public class InfiniteViewPagerTest extends Activity
 		}
 
 		@Override
-		public int getCount()
-		{
-			return 10;
-		}
-
-		@Override
 		public boolean isViewFromObject(View view, Object object)
 		{
 			return view == object;
